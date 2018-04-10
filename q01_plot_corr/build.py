c8f43cac67deeed917d40599cdfcd8001eb27c27
sonal7930 <sonal.chandra7930@gmail.com>
2018-04-10 16:39:11 +0000

    Done

--- q01_plot_corr/build.py
+++ q01_plot_corr/build.py
@@ -1,5 +1,7 @@
+# %load q01_plot_corr/build.py
 # Default imports
 import pandas as pd
+import matplotlib as plt
 from matplotlib.pyplot import yticks, xticks, subplots, set_cmap
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
@@ -9,8 +11,10 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 def plot_corr(data, size=11):
     corr = data.corr()
     fig, ax = subplots(figsize=(size, size))
-    set_cmap("YlOrRd")
+    set_cmap('YlOrRd')
     ax.matshow(corr)
     xticks(range(len(corr.columns)), corr.columns, rotation=90)
     yticks(range(len(corr.columns)), corr.columns)
     return ax
+
+
